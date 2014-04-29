5669cbd293b1a72ecae5a05c7b23fc1c0c30d183
eukaryote <sapientdust+github@gmail.com>
2014-04-28 20:53:26 -0700

    strip string values before insertion

--- knowhow/index.py
+++ knowhow/index.py
@@ -32,7 +32,7 @@ class Index:
     def _add(self, writer, **kwargs):
         kwargs['id'] = identifier(kwargs)
         kwargs['updated'] = datetime.utcnow()
-        writer.update_document(**kwargs)
+        writer.update_document(**strip(kwargs))
 
     def add(self, **kwargs):
         with self.ix.writer() as w:
@@ -52,3 +52,25 @@ class Index:
     def search(self, qs):
         parser = QueryParser('content', self.ix.schema)
         return self.query(parser.parse(qs))
+
+
+def strip(val):
+    """
+    Recursively strip whitespace from strings and arbitrarily nested
+    strings in dicts or iterable containers, preserving all container types
+    (e.g., tuples remain tuples in output, lists remain list in output,
+    etc.). Any non-standard (non-dict) iterable types must have an
+    `__init__` that accepts a list as the first parameter for creation
+    of a new instance, just as `list` and `tuple` themselves do.
+    """
+    # Not very efficient, but it's nice to be able to preserve types;
+    # of course, not needed above at all, since the values above
+    # can only be lists that are not nested.
+    if isinstance(val, str):
+        return val.strip()
+    elif isinstance(val, dict):
+        return type(val)({k: strip(val[k]) for k in val})
+    try:
+        return type(val)(map(strip, val))
+    except TypeError:  # not iterable
+        return val     # everything else returned as is
