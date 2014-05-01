1112a524c0d8b4890e8a72ae3a9180659dbb1274
eukaryote <sapientdust+github@gmail.com>
2014-05-01 08:12:06 -0700

    cleanup

--- bin/info.py
+++ bin/info.py
@@ -21,16 +21,16 @@ from knowhow.index import Index
 
 
 def print_info(reader):
+    print('%d snippet(s)' % reader.doc_count())
     tags = (t.decode('utf-8') for t in reader.lexicon('tag'))
-    print('tags:', ', '.join(tags))
-    print('tag counts:')
-    print('  ', reader.most_frequent_terms('tag'))
+    print('all tags:', ', '.join(tags))
+    print('most frequent tags:')
+    for count, term in reader.most_frequent_terms('tag'):
+        print('    {0}: {1}'.format(count, term))
 
 
 def main(args):
-    index = Index()
-    print('%d snippet(s)' % len(index))
-    with index.ix.reader() as reader:
+    with Index().ix.reader() as reader:
         print_info(reader)
     return 0
 
