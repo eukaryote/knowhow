c37d5764f2bbef448c9113e62ab42e0511668ba0
eukaryote <sapientdust+github@gmail.com>
2015-02-17 05:33:57 -0800

    version info and docstring

--- knowhow/__init__.py
+++ knowhow/__init__.py
@@ -1,7 +1,13 @@
-#-*- coding: utf-8 -*-
+# coding=utf8
+
 """
-A library and app for storing and searching development knowledge
-while working in a shell, text editor, IDE, etc.
+knowhow is a searchable and scriptable knowledge repository for useful snippets
+of information that are worth saving for future reference. It consists of a
+library for programmatic use, and a commandline script for interactive usage.
+
+It will certainly change in major ways that are backwards-incompatible, and
+is not intended yet for general use.
 """
 
-__version__ = '0.1'
+__version_info__ = (0, 1, 0)
+__version__ = '.'.join(map(str, __version_info__))
